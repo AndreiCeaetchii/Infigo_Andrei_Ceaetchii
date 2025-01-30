7cd464a4cf0f9928429839c90b16b00d4d6e03c0
Andrei CEAETCHII <ceaetchiiandrei@gmail.com>
2025-01-30 17:22:29 +0200

    Add CommentRepository

---
+++ CMSPlus.Domain/Repositories/CommentRepository.cs
@@ -0,0 +1,19 @@
+﻿using CMSPlus.Domain.Entities;
+using CMSPlus.Domain.Interfaces;
+using CMSPlus.Domain.Persistance;
+using Microsoft.EntityFrameworkCore;
+
+namespace CMSPlus.Domain.Repositories;
+
+public class CommentRepository:Repository<CommentEntity>,ICommentRepository
+{
+    public CommentRepository(ApplicationDbContext context) : base(context)
+    {
+    }
+
+    public async Task<IEnumerable<CommentEntity>> GetCommentsByTopicId(int topicId)
+    {
+        var result = await _dbSet.Where(comment => comment.TopicId == topicId).ToListAsync();
+        return result;
+    }
+}
